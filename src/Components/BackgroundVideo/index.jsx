36348aaf4dcb12d111ea3b4a727eb49095b8a522
angel-ayaya <angel.ayala@upnay.edu.mx>
2024-06-27 00:46:47 -0700

    feat: Update BackgroundVideo component with new video source

--- src/Components/BackgroundVideo/index.jsx
+++ src/Components/BackgroundVideo/index.jsx
@@ -7,7 +7,7 @@ const BackgroundVideo = () => {
   return (
     <VideoContainer>
       <video autoPlay loop muted>
-        <source src={background} type="video/mp4" />
+        <source src="https://download1326.mediafire.com/6thjw27boojgfl7FmJ4WYpwXGetTsQkOomfp7WpBjeCnEicEknBXg_vYMa3amDDD44ItNrFJjY4Jd6j130oaddbW6TyAfa1zXRQvHhIO6mpmSonOhgzQUMArHpZ4nA-74SsNZSI1w9qFfxHs_QSZ58Dt2vfGWLh2tthHolyKdkYykN0/edont8o41dhqlek/background_akali.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </VideoContainer>
