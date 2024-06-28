c6a2a400518c78d83c6a2a354e02b4301807a5b9
angel-ayaya <angel.ayala@upnay.edu.mx>
2024-06-27 21:23:41 -0700

    refactor: Update BackgroundVideo component with new video source

--- src/Components/BackgroundVideo/index.jsx
+++ src/Components/BackgroundVideo/index.jsx
@@ -1,13 +1,13 @@
 // src/components/BackgroundVideo.js
 import React from "react";
 import styled from "styled-components";
-import background from "../../utils/background_akali.mp4";
+import Background from "../../utils/background.mp4";
 
 const BackgroundVideo = () => {
   return (
     <VideoContainer>
       <video autoPlay loop muted>
-        <source src="https://download1326.mediafire.com/6thjw27boojgfl7FmJ4WYpwXGetTsQkOomfp7WpBjeCnEicEknBXg_vYMa3amDDD44ItNrFJjY4Jd6j130oaddbW6TyAfa1zXRQvHhIO6mpmSonOhgzQUMArHpZ4nA-74SsNZSI1w9qFfxHs_QSZ58Dt2vfGWLh2tthHolyKdkYykN0/edont8o41dhqlek/background_akali.mp4" type="video/mp4" />
+        <source src={Background} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </VideoContainer>
