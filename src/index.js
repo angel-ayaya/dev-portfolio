7248af06786e2b3e17e67264b73cc2d1df80e353
angel-ayaya <angel.ayala@upnay.edu.mx>
2024-06-27 22:35:22 -0700

    refactor: Update react-router-dom usage in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,19 +8,29 @@ import Calculator from "./Pages/Calculator";
 import ToDo from "./Pages/ToDo";
 import Substract from "./Pages/Substract";
 
-const Router = process.env.REACT_APP_ROUTER_MODE === 'hash' ? HashRouter : BrowserRouter;
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/calculator",
+    element: <Calculator />,
+  },
+  {
+    path: "/todo",
+    element: <ToDo />,
+  },
+  {
+    path: "/substract",
+    element: <Substract />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/todo" element={<ToDo />} />
-        <Route path="/substract" element={<Substract />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
