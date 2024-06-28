9a74162bd6b79fb3b6b042fef54388f2bfa608bc
angel-ayaya <angel.ayala@upnay.edu.mx>
2024-06-27 21:42:59 -0700

    feat: Update Substract page with new container class

--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Calculator from "./Pages/Calculator";
 import ToDo from "./Pages/ToDo";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/calculator",
-    element: <Calculator />,
-  },
-  {
-    path: "/todo",
-    element: <ToDo />,
-  },
-]);
+import Substract from "./Pages/Substract";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/todo" element={<ToDo />} />
+        <Route path="/substract" element={<Substract />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
